8983ca4201048465973e128a6f294869968abf9d
yitan1 <yitam98@gmail.com>
2023-09-22 10:42:00 +0800

    up part save

--- src/io.jl
+++ src/io.jl
@@ -16,8 +16,19 @@ function get_es_name(params, px, py)
     dir = get_dir(params)
     # TODO
     # px, py = string(px), string(py)
-    es_name = "$(dir)/es_$(px)_$(py).jld2"
-    
+    if params["part_save"] 
+        es_dir = "$(dir)/es_$(px)_$(py)"
+        if ! ispath(es_dir)
+            println("$es_dir does not exist, and will be created")
+            mkpath(es_dir)
+        end
+        st = params["es_resume"]
+        ed = st + params["es_num"] - 1
+        es_name = "$es_dir/$(st)_$(ed).jld2"
+    else
+        es_name = "$(dir)/es_$(px)_$(py).jld2"
+    end
+
     es_name
 end
 
