d5e31fb034ba9bbaeed134e5502c07630e74e88a
yitan1 <yitam98@gmail.com>
2024-01-21 20:03:18 +0800

    fix plot

--- src/plot.jl
+++ src/plot.jl
@@ -55,7 +55,7 @@ function basis_dep(n, px, py, filename::String)
 end
 
 function plot_spectral(es, swk0; step = 0.1, factor = 0.04, x_max = 0)
-    if max > 0
+    if x_max > 0
         x_max = x_max
     else
         x_max = ceil(maximum(es))
